3e8aceb2fc73486b87e0da0017bf0fc9cf246c1b
Brian Hardell <bharde@me.com>
2020-05-08 18:26:08 -0700

    protect API key

--- public/alarm.js
+++ public/alarm.js
@@ -1,14 +1,3 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
-};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
